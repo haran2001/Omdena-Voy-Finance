b21b1daa917fe4f879c20797246d11a7a86291dc
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-07 17:08:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ def load_model_from_gd(model_name):
     # f_checkpoint = Path(f"models//{model_name}")
     with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
         # gdown.download(id='1--eYkRRQl6CAuXxPFcgiFy0zdp67WTPE', output=output, quiet=False)
-        gdown.download(id='1OCc8IBYaZdklXMXOsnsMYU3k1Ld95uSs', output=output, quiet=False)
+        gdown.download(id='1--OCc8IBYaZdklXMXOsnsMYU3k1Ld95uSs', output=output, quiet=False)
 
 def get_class(image, newsize, MODEL):
     image = image.resize(newsize)

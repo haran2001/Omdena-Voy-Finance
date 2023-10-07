2068ae655f0519433c1196ad0d6504c43b614e61
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-07 12:07:08 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,16 +6,9 @@ import streamlit as st
 import subprocess
 import os
 import urllib.request
-
 from tensorflow.keras.models import load_model
 import gdown
 from pathlib import Path
-
-model_name = 'withouth_cersc_resnet50_deduplicated_mix_val_train_75acc.h5'
-
-save_dest = Path('models')
-save_dest.mkdir(exist_ok=True)
-output = f'models/{model_name}'
 
 @st.cache_resource
 def load_model_h5(path):
@@ -25,15 +18,6 @@ def load_model_from_gd():
     #f_checkpoint = Path(f"models//{model_name}")
     with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
         gdown.download(id='1--eYkRRQl6CAuXxPFcgiFy0zdp67WTPE', output=output, quiet=False)
-
-#Function to get prediction array for a model (used in ensembling)
-def get_all_predictions(model, img):
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-
-    predictions = model.predict(img_array)
-
-    return predictions[0]
 
 def get_class(image, newsize, MODEL):
     image = image.resize(newsize)
@@ -51,6 +35,15 @@ def get_class(image, newsize, MODEL):
     confidence = np.max(predictions[0])
 
     return predicted_class, confidence
+    
+#Function to get prediction array for a model (used in ensembling)
+def get_all_predictions(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    return predictions[0]
 
 def get_class_ensemble(image, newsize, MODEL_A, MODEL_B):
     image = image.resize(newsize)
@@ -73,7 +66,7 @@ def get_class_ensemble(image, newsize, MODEL_A, MODEL_B):
     return predicted_class_ensemble, confidence_ensemble
     
 #Function to get final predictions
-def predict():
+def predict(size, MODEL):
     image = None
     upload_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
     upload_camera = st.camera_input("Or take a picture here...")
@@ -86,24 +79,16 @@ def predict():
         
     if image is not None:
         st.image(image)
-        newsize = (256, 256)
-        newsize1 = (256, 256)
-        newsize3 = (224, 224)
-        newsize4 = (256, 256)
-
-        predicted_class1, confidence1 = get_class(image, newsize1, MODEL1)
-        # predicted_class4, confidence4 = get_class(image, newsize4, MODEL4)
-        predicted_class4, confidence4 = get_class(image, newsize3, MODEL3)
-    
-        predicted_class_ensemble, confidence_ensemble = get_class_ensemble(image, newsize, MODEL1, MODEL4)
+        predicted_class, confidence = get_class(image, newsize1, MODEL1)
+        # predicted_class_ensemble, confidence_ensemble = get_class_ensemble(image, newsize, MODEL1, MODEL4)
         
-        predicted_class_ensemble = None
-        confidence_ensemble = None
+        # predicted_class_ensemble = None
+        # confidence_ensemble = None
 
         
-        return {"class1": predicted_class1, "confidence1": float(confidence1), "class4": predicted_class4, "confidence4": float(confidence4), "class_ensemble": predicted_class_ensemble, "confidence_ensemble": confidence_ensemble}
+        return {"class": predicted_class, "confidence": float(confidence)}
     else:
-        return {"class1": "No Image", "confidence1": 0, "class4": "No Image", "confidence4": "No Image", "class_ensemble": "No Image", "confidence_ensemble": "No Image"}
+        return {"class": "No Image", "confidence": 0, "class4": "No Image"}
 
 
 
@@ -117,16 +102,30 @@ MODEL1 = tf.keras.models.load_model("model_CNN1_BRACOL.h5", compile=False)
 # Mobilenet-v2 
 MODEL4 = tf.keras.models.load_model("Omdena_model4.h5", compile=False)
 
+# Resnet-v2
+model_name = 'withouth_cersc_resnet50_deduplicated_mix_val_train_75acc.h5'
+save_dest = Path('models')
+save_dest.mkdir(exist_ok=True)
+output = f'models/{model_name}'
 f_checkpoint = Path(f"models//{model_name}")
+
 if not f_checkpoint.exists():
     load_model_from_gd()
 else:
     MODEL3 = load_model_h5(f_checkpoint)
-    
-predicted_output = predict()
+
+newsize  = (256, 256)
+newsize1 = (256, 256)
+newsize3 = (224, 224)
+newsize4 = (256, 256)
+
 st.write("Model Predictions: ")
-st.write("Prediction from Cusomized CNN (BRACOL symptoms): ", predicted_output['class1'])
-st.write("Prediction from Mobilenet-v2 (2667589 parameters): ", predicted_output['class4'])
-st.write("Prediction from Ensemble of Cusomized CNN (BRACOL symptoms) and mobilenet-v2 : ", predicted_output['class_ensemble'])
+predicted_output1 = predict(newsize1, MODEL1)
+st.write("Prediction from Cusomized CNN (BRACOL symptoms): ", predicted_output1['class'])
 
+predicted_output3 = predict(newsize3, MODEL3)
+st.write("Prediction from Mobilenet-v2 (2667589 parameters): ", predicted_output1['class'])
 
+predicted_output4 = predict(newsize4, MODEL4)
+st.write("Prediction from Mobilenet-v2 (2667589 parameters): ", predicted_output1['class'])
+# st.write("Prediction from Ensemble of Cusomized CNN (BRACOL symptoms) and mobilenet-v2 : ", predicted_output['class_ensemble'])

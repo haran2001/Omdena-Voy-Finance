4f8d68cf56d940d8a9e4690e8e008c91322d61c1
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-08 12:24:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,26 +1,23 @@
-from helping_functions import load_model_h5, load_model_from_gd, predict, predict_ensemble, get_image
+from helping_functions import load_model_h5, load_model_from_gd, predict, predict_ensemble, get_image, verify_checkpoint
 import streamlit as st
 from pathlib import Path
 
 # Custom CNN    
 MODEL1 = load_model_h5('assets/models/model_CNN1_BRACOL.h5')
+
 # Sequential CNN -v2 
 MODEL2 = load_model_h5('assets/models/Omdena_model1.h5')
+
 # Mobilenet-v2 
 MODEL4 = load_model_h5('assets/models/Omdena_model4.h5')
+
 # Resnet-v2
-model_name = 'withouth_cersc_resnet50_deduplicated_mix_val_train_75acc.h5'
-f_checkpoint = Path(f"models//{model_name}")
-
-if not f_checkpoint.exists():
-    load_model_from_gd(model_name)
-
-MODEL3 = load_model_h5(f_checkpoint)
+if verify_checkpoint('withouth_cersc_resnet50_deduplicated_mix_val_train_75acc.h5'):
+    MODEL3 = load_model_h5(f_checkpoint)
 
 #Resize requirements
 newsize  = (256, 256)
 newsize1 = (256, 256)
-# newsize3 = (256, 256)
 newsize3 = (224, 224)
 newsize4 = (256, 256)
 

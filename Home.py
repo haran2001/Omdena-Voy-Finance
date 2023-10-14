dd7dbe3d66068ceb993798c43ba8d2ec3b60b89a
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-14 15:04:52 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ from keras import backend as K
 st.set_page_config(page_title='Plants diseases', page_icon=':herb:', initial_sidebar_state='auto')
 st.header('Omdena: Sao Paulo Chapter')
 st.subheader('Plant Disease Classificattion')
-st.write('The following application will help you identify the disease infecting a plant, if ou provide an image')
+st.write('The following application will help you identify the disease infecting a plant, if you provide an image')
 st.write('You can provide an image in one f te following ways: ')
 st.write('1. Upload an existing photo')
 st.write('2. Take a photo through our camera')

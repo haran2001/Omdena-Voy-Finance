f419200d786e47ecc5272dee3241aeb14185a441
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-14 17:19:29 +0530

    Update Details.py

--- pages/Details.py
+++ pages/Details.py
@@ -15,17 +15,26 @@ st.markdown('''
 
 col1, col2 = st.columns([2,1])
 with col1:
-    st.subheader("Flowchart")
-    flowchart = Image.open('assets/images/flowchart.png')
+    st.subheader("Siamese network (MobileNetV2 architecture)")
+    flowchart = Image.open('assets/images/image6.png').resize((128, 128))
     st.image(flowchart)
 with col2:
     st.subheader("Steps")
-    st.write("**Image Augmentation:** Existing images of the minority class are augmented to create new images, \
-        increasing the dataset size.")
-    st.write("**Cropping Images:** All images are cropped to remove irrelevant pixels and reduce noise.")
-    st.write("**Transfer Learning:** Preprocessed images are passed through a pre-trained neural network to extract features.")
-    st.write("**Classification:** The extracted features are stored in a pandas dataframe and used to train different \
-        classifiers for image classification.")
+    st.write("The Siamese Network architecture is used for the coffee disease classification using images. \
+    The main goal for using this network is to build an embedding space where the different classes are separated into distinct clusters. \
+    The siamese network architecture is used to generate feature embeddings such that intra-class image features are close to each other in the embedding space, \
+    whereas the inter-class image features are further apart from each other. \
+    The motivation behind this setup is to allow diverse input images that are semantically similar to be close to each other. \
+    In the coffee dataset, we have single leaf images indoors on a white background under different lighting conditions, \
+    as well as images outdoors in natural light on the plant. \
+    Siamese networks capture this diversity while still keeping the features close to each other in the embedding space. \
+    To measure the distance or closeness in embedding space, we use similarity metrics like cosine similarity, euclidean distance, and dot product. \
+    For the coffee disease classification problem the euclidean distance measures the closeness between image pairs, which in technical terms is also called the similarity metric. \
+    The advantage of using this method is 2-fold: \
+    One can use a simple K-nearest neighbour approach to classify the input image. \
+    The setup can visually explain why the model chose the particular class. \
+    The following K-NN strategy is used for inference: \
+    ")
     
 st.subheader("Data Imbalance")
 col3, col4 = st.columns([1,2])

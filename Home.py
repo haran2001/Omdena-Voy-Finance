1fa17c939a5201d52300e580e8cc4d1e8e176b06
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-14 11:47:59 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,8 @@ model3 = 'withouth_cersc_resnet50_deduplicated_mix_val_train_75acc.h5'
 f_checkpoint = Path(f"assets/models//{model3}")
 if verify_checkpoint(model3, f_checkpoint):
     # MODEL3 = load_model_h5(f_checkpoint)
-    MODEL3 = load_model(f_checkpoint, custom_objects={"K": K})
+    # MODEL3 = load_model(f_checkpoint, custom_objects={"K": K})
+    MODEL3 = load_model(f_checkpoint)
 
 
 #Resize requirements

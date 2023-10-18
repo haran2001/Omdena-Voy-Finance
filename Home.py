0fed6d5150436cbe6ebc86d25c039c36aca04b5f
dots_13 <stasya.dots@gmail.com>
2023-10-19 00:40:10 +0200

    add pytorch class to Home page

--- Home.py
+++ Home.py
@@ -13,6 +13,49 @@ st.write('1. Upload an existing photo')
 st.write('2. Take a photo through your camera')
 st.write('Once done press the classifiy button to the results from each of our models')
 
+# PyTorch Model
+class CoffeeLeafClassifier(nn.Module):
+    def __init__(self):
+        super(CoffeeLeafClassifier, self).__init__()
+        
+        # Convolutional layers
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(32, 64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(64, 128, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        
+        # Fully connected layers
+        self.fc_layers = nn.Sequential(
+            nn.Linear(128 * 30 * 30, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            
+            nn.Linear(128, 5) # 5 classes
+        )
+        
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1) # Flatten the output
+        x = self.fc_layers(x)
+        return x
+
 # Custom CNN    
 MODEL1 = load_model_h5('assets/models/model_CNN1_BRACOL.h5')
 
@@ -21,7 +64,6 @@ MODEL2 = load_model_h5('assets/models/Omdena_model1.h5')
 
 # Mobilenet-v2 
 MODEL4 = load_model_h5('assets/models/Omdena_model4.h5')
-
 
     
 # CNN Pytorch model

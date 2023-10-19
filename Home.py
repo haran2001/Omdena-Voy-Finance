e12b0e901b192cd6616f74187479d16ec716099c
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-10-19 07:20:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 st.set_page_config(page_title='Plants diseases', page_icon=':herb:', initial_sidebar_state='auto')
 st.header('Omdena: Sao Paulo Chapter', anchor=False)
-st.subheader('Plant Disease Classification', anchor=False)
+st.subheader('Coffee Plant Disease Classification', anchor=False)
 st.write('The following application will help you identify the disease infecting a plant, if you provide an image os the leaf.')
 st.write('You can provide an image in one of the following ways: ')
 st.write('1. Upload an existing photo')

236be613d4eda289952ac69bcf3f118562c382cd
“haran2001” <hari.ayapps@gmail.com>
2024-01-05 21:58:22 +0530

    linkedin updates

--- Home.py
+++ Home.py
@@ -25,28 +25,16 @@ st.write(
     "The following application will help you assign a Digital score to a company, if you provide the company's name."
 )
 
-AVG = 951938.0811764706
-frequency_ratio = 2.09 / 3.05
+# AVG = 951938.0811764706
+# frequency_ratio = 2.09 / 3.05
 
 name = st.text_input("Enter the company name 👇")
-df = pd.read_csv("assets/csv/DDS.csv")
-
-# if True:
-# try:
-# fb_url = get_facebook_url(name)
-# likes_, followers_ = get_likes_followers(fb_url)
-# followers_ = text_to_num(followers_.split(" ")[0])
-# digital_score = followers_ * frequency_ratio / AVG
-# st.write("Digital Score: ", digital_score)
-# except Exception as e:
-# likes_, followers_ = 0, 0
-# digital_score = followers_ * frequency_ratio / AVG
-# st.write("Digital Score: ", digital_score)
+df = pd.read_csv("assets/csv/Digital_score_linkedin.csv")
 
 if name:
     try:
-        digital_score = df[df["Name"] == name]["digital_score"].values[0]
-        ws_rank = df[df["Name"] == name]["ws_ranks"].values[0]
+        digital_score = df[df["Company"] == name]["digital_score"].values[0]
+        ws_rank = df[df["Company"] == name]["ws_rank"].values[0]
         st.write("Digital Score: ", digital_score)
         st.write(
             "The digital score reflects how large a company's active social media following compared to an average fortune 1000 company. Here ",
